32288718d519786f281509aa02224c1071f1a830
leandro <leandromarcosdedeus@gmail.com>
2024-12-28 18:10:08 -0300

    retirando comentario desnecessários

--- Consumidor.py
+++ Consumidor.py
@@ -8,12 +8,10 @@ class Consumidor:
 
     def consumir(self, estoque):
         while True:
-            # Tempo de consumo aleatório entre 1 e 3 segundos
             tempo_espera = random.randint(1, 3)
             time.sleep(tempo_espera)
 
             item = estoque.remover()
 
             if item is None:
-                # Aguarda se o estoque estiver vazio
                 time.sleep(1)
--- Produtor.py
+++ Produtor.py
@@ -5,15 +5,13 @@ import random
 class Produtor:
     def __init__(self, identificador):
         self.identificador = identificador
-        self.contador = 1  # Contador para identificar os itens produzidos
+        self.contador = 1  
 
     def produzir(self, estoque):
         while True:
-            # Tempo de produção aleatório entre 1 e 5 segundos
             tempo_espera = random.randint(1, 5)
             time.sleep(tempo_espera)
             
-            # Criando um item
             item = (self.identificador, self.contador)
             sucesso = estoque.estocar(item)
             
